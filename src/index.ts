2bc73dd9eac3c5a2d69afc48717fbdb972bbc87d
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-01-19 10:55:58 +0530

    Other routes setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,137 @@
-import express from "express";
+import express from "express"
+import cors from "cors";
 import { config } from "dotenv";
+import { PrismaClient } from "@prisma/client";
+import * as RecipeAPI from "./recipe-api";
 
 config()
 
 const app = express();
-
 const PORT = process.env.PORT || 4000;
+const prismaClient = new PrismaClient();
+
+app.use(express.json())
+app.use(cors());
+
+
+
+app.get("/api/recipes/search", async (req, res) => {
+    // https://api.spoonacular.com/recipes/serach?searchTerm=burger&page=2
+
+    const searchTerm = req.query.searchTerm as string;
+    const page = parseInt(req.query.page as string);
+
+    const results = await RecipeAPI.searchRecipes(searchTerm, page);
+
+    return res.json(results);
+})
+
+app.get("/api/recipes/:recipeId/summary", async (req, res) => {
+
+    const recipeId = req.params.recipeId;
+    const results = await RecipeAPI.getRecipeSummary(recipeId)
+
+    return res.json(results);
+});
+
+app.post("/api/recipes/favourite", async (req, res) => {
+    const recipeId = req.body.recipeId;
+
+    try {
+        const favRecipe = await prismaClient.favResipies.create(
+            {
+                data: {
+                    recipeId
+                }
+            }
+        );
+        return res.status(201).json(favRecipe)
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Oops! Something went wrong" })
+    }
+})
+
+app.get("/api/recipes/favourite", async (_ , res) => {
+    try {
+        const favRecipes = await prismaClient.favResipies.findMany();
+
+        const recipeIds = favRecipes.map((recipe: { recipeId: number }) => recipe.recipeId.toString())
+
+        const favorites = await RecipeAPI.getFavRecipedByIDs(recipeIds);
+
+        return res.json(favorites);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+
+    }
+
+})
+
+app.delete('/api/recipes/favourite', async (req, res) => {
+    const recipeId = req.body.recipeId;
+    try {
+        await prismaClient.favResipies.delete({
+            where: {
+                recipeId: recipeId
+            }
+        })
+        return res.status(204).send();
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Oops! Something went wrong" })
+    }
+})
+
+
+app.post("/chat", async (req, res) => {
+    const { prompt, servings, dishType, spiceLevel, allergyType } = req.body;
+    try {
+        const chatGptResponse = await RecipeAPI.chatGpt(prompt, servings, dishType, spiceLevel, allergyType);
+        return res.json({ chatGptResponse });
+
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+});
+
+
+app.post("/api/recipes/favourite", async (req, res) => {
+    const recipeId = req.body.recipeId;
+
+    try {
+        const favRecipe = await prismaClient.favResipies.create(
+            {
+                data: {
+                    recipeId
+                }
+            }
+        );
+        return res.status(201).json(favRecipe)
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Oops! Something went wrong" })
+    }
+})
+
+app.get("/api/recipes/favourite", async (_ , res) => {
+    try {
+        const favRecipes = await prismaClient.favResipies.findMany();
+
+        const recipeIds = favRecipes.map((recipe: { recipeId: number }) => recipe.recipeId.toString())
+
+        const favorites = await RecipeAPI.getFavRecipedByIDs(recipeIds);
+
+        return res.json(favorites);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ error: "Internal Server Error" });
+
+    }
+
+})
 
 app.get('/', (_, res) => {
     res.send("<h1>Hello Dev!</h1>")
---
+++ types.ts
@@ -0,0 +1,18 @@
+import { chatGpt } from './src/recipe-api';
+export interface Recipe {
+    id: number;
+    title: string;
+    image: string;
+    imageType: string
+}
+
+export interface RecipeSummary {
+    id: number;
+    title: string;
+    summary: string;
+}
+
+export interface chatGpt {
+    prompt: string,
+    chatGptResponse: string
+}
