f9ad4186300c07dc995d201cfb6c6f9b687b295a
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-01-19 11:21:55 +0530

    cors origin url

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,11 @@ const PORT = process.env.PORT || 4000;
 const prismaClient = new PrismaClient();
 
 app.use(express.json())
-app.use(cors());
-
+app.use(cors({
+    origin: ["https://spoonful-saga-joycvru4h-yadavnishant2020.vercel.app/"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }));
 
 
 app.get("/api/recipes/search", async (req, res) => {
